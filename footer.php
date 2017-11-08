b6a87386dcb19a1adcd1917f7ec7daa0f1f70161
LChord <569187749@qq.com>
2017-11-09 01:20:48 +0800

    Add post nav, footer info

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 <footer id="footer">
 <section class="foot-warp">
  <section class="copyright">
+ <span>
   &copy; <?php echo date('Y') ?> <?php bloginfo( 'name' ); ?> 
   <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" rel="nofollow">CC BY-NC-SA 4.0</a>
+  </span>
+  <br>
+  <span>Theme by <a href="https://www.giuem.com/">Giuem</a> &amp; <a href="https://yuusan.xyz">Yuusan</a></span> 
  </section>
 </section>
 </footer>
--- single.php
+++ single.php
@@ -1,26 +1,31 @@
 <?php get_header();?>
-<section id="content">
-   <main id="main">
-   <?php while ( have_posts() ) : the_post(); ?>
-    <article class="post">
-     <header class="post-head">
-      <h1 class="post-title"><?php the_title();?></h1>
-      <time class="post-time"><time><?php echo date('M j Y',get_the_time('U'));?></time></time>
-     </header>
-     <div class="post-content">
+    <section id="content">
+        <main id="main">
+        <?php while ( have_posts() ) : the_post(); ?>
+        <article class="post">
+        <header class="post-head">
+        <h1 class="post-title"><?php the_title();?></h1>
+        <time class="post-time"><time><?php echo date('M j Y',get_the_time('U'));?></time></time>
+        </header>
+        <div class="post-content">
         <?php the_content();?>
-     </div>
-     <footer class="post-foot"></footer>
-     <?php the_post_navigation( array(
-                    'next_text' => '<span class="meta-nav">Next</span><span class="post-title">%title</span>',
-                    'prev_text' => '<span class="meta-nav">Prev</span><span class="post-title">%title</span>',
-                ) );?>
-     <section id="comment">
+        </div>
+        </article>
+        <nav class="page-nav clear">
+        <?php the_post_navigation( array(
+            'next_text' => '<span class="prev"><span class="meta-nav"> « </span><span class="post-title">%title</span></span>',
+            'prev_text' => '<span class="next"><span class="post-title">%title</span><span class="meta-nav"> » </span></span>',
+        ) );?>
+        </nav>
+        <section id="comment">
         <?php
         if ( comments_open() || get_comments_number() ) :
-            comments_template();
+        comments_template();
         endif;
         ?>
-     </section>   
+        </section>   
         <?php endwhile; ?>
+        </main>
+    </section>
+<?php get_footer();?>   
 
