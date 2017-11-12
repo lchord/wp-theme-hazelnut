ad6c74b3215ffb1d361757007e33a7bc67be821e
lizhe <569187749@qq.com>
2017-11-12 22:16:33 +0800

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header();?>
+	<main class="main">
+	<div class="not-found">
+        <b>404</b>
+        <p>没找到任何内容，返回<a href="<?php echo home_url();?>">首页</a></p>
+	</div>
+	</main>
+<?php get_footer();?>
